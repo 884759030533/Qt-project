675bfd356af601bfdeea98d82fe5df1df797e321
F <777frozenflame777@gmail.com>
2021-07-08 11:17:51 +0300

    ye

--- AspireProject/tile.cpp
+++ AspireProject/tile.cpp
@@ -1,6 +1,52 @@
 #include "tile.h"
 
-Tile::Tile()
+Tile::Tile(long X, long Y, long Z)
+{
+
+}
+
+Tile::Tile(tPosition Pos)
+{
+
+}
+
+Tile::Tile(long X, long Y, long Z, unsigned int Height, unsigned int Width, bool Solid, ImageType &Image)
+{
+
+}
+
+Tile::Tile(tPosition Pos, tSize Size)
+{
+
+}
+
+Tile::~Tile()
+{
+
+}
+
+void Tile::Draw()
+{
+
+}
+
+void Tile::Move(tPosition destination, int speed)
+{
+
+}
+
+void Tile::Move(tPosition destination, moveFunction moveFunction)
+{
+
+}
+
+
+void AnimatedTile::Draw()
+{
+
+}
+
+void Slope::Draw()
 {
 
 }
--- AspireProject/tile.h
+++ AspireProject/tile.h
@@ -6,33 +6,74 @@ struct tPosition //tile position in grid
     long x;
     long y;
     long z;
+    short layer;
     tPosition(): x(0), y(0), z(0) {};
-    tPosition(long X, long Y, long Z): x(X), y(Y), z(Z) {};
+    tPosition(long X, long Y): x(X), y(Y), z(0), layer(1) {};
+    tPosition(long X, long Y, long Z): x(X), y(Y), z(Z), layer(1) {};
+    tPosition(long X, long Y, long Z, short Layer): x(X), y(Y), z(Z), layer(Layer) {};
+
 };
-struct tileSize //size of a tile (for proper texture positioning)
+struct tSize //size of a tile (for proper texture positioning)
 {
     unsigned int height;
     unsigned int width;
-    tileSize(): height(10), width(10) {};
-    tileSize(unsigned int Height, unsigned int Width): height(Height), width(Width) {};
+    tSize(): height(10), width(10) {};
+    tSize(unsigned int Height, unsigned int Width): height(Height), width(Width) {};
 };
-struct ImageType {};
+struct ImageType {}; //need image type
+struct GifType {}; //need gif-image type
+typedef int (*moveFunction)(tPosition, int);
 
 class Tile
 {
 private:
     tPosition pos;
-    int layer;
-    bool solid; // can player collide with current tile
-    tileSize size;
+    tSize size;
     ImageType img;
-
+protected:
+    bool solid; // can player collide with current tile
+    bool dealDamage;
+    int damage;
 public:
-    Tile(long X, long Y, long Z); //quick spawn tile
-    Tile(tPosition Pos); //quick spawn tile
-    Tile(long X, long Y, long Z, unsigned int Height, unsigned int Width, bool Solid, ImageType Image); //spawns regular tile
-    Tile(tPosition Pos, tileSize Size); //spawns regular tile
+    Tile(long x, long y, long z); //quick spawn tile
+    Tile(tPosition pos); //quick spawn tile
+    Tile(long x, long y, long z, unsigned int height, unsigned int width, bool isSolid, ImageType &image); //spawns regular tile
+    Tile(tPosition pos, tSize size); //spawns regular tile
     ~Tile();
+    virtual void Draw();
+    void Move(tPosition destination, int speed); //simple tile move
+    void Move(tPosition destination, moveFunction moveFunction); //moving tile with different animation
+
+};
+
+class AnimatedTile : public Tile
+{
+private:
+    GifType gif;
+public:
+    AnimatedTile(tPosition pos, tSize size, GifType &gif) : Tile(pos, size)
+    {
+        this->gif = gif;
+    }
+    AnimatedTile(tPosition pos, tSize size,  bool isSolid, GifType &gif) : Tile(pos, size)
+    {
+        this->gif = gif;
+        this->solid = isSolid;
+    }
+    void Draw();
+
+};
+
+class Slope : public Tile
+{
+private:
+    short slopeType;
+public:
+    Slope(tPosition pos, tSize size, short slopeType) : Tile(pos, size)
+    {
+        this->slopeType = slopeType;
+    }
+    void Draw();
 
 };
 
